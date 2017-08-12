1644ab8f86cca0e4e5eadad8aece720e774a1d99
brentli1 <brent.jlinville@gmail.com>
2017-08-11 22:22:23 -0500

    add font-awesome; add search icon to search bar; various style updates

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,6 +10,7 @@ class SearchBar extends Component {
   render() {
     return (
       <div className="search-bar">
+        <i className="fa fa-search"></i>
         <input
           value={ this.state.term } 
           onChange={ event => this.onInputChange(event.target.value) } 
